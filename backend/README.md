88f9ce3f382f2bb5c03e205cc61e7e3aa620f36d
Karina008 <30279913+Karina008@users.noreply.github.com>
2020-11-17 01:45:28 +0900

    IMS
    IMS

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Backend Project
+
+### 서울 1반 1팀 IMS Backend
---
+++ backend/src/main/java/com/ssafy/IMS/model/Essay.java
@@ -0,0 +1,52 @@
+package com.ssafy.IMS.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "essay")
+public class Essay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @NotBlank
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "result1")
+    private String result1;
+    @Column(name = "result2")
+    private String result2;
+    @Column(name = "result3")
+    private String result3;
+    @Column(name = "result4")
+    private String result4;
+    @Column(name = "result5")
+    private String result5;
+    @Column(name = "result6")
+    private String result6;
+    @Column(name = "result7")
+    private String result7;
+    @Column(name = "result8")
+    private String result8;
+
+    @Column(name = "share")
+    private String share;
+
+}
